09db20c2943aa7a147d98c31fb0a4f5c6e112118
cavonLucky <425247833@qq.com>
2022-04-12 22:16:09 +0800

    7-3使用styled-commponents完成Header组件布局（2）

--- src/common/ header/ index.js
+++ src/common/ header/ index.js
@@ -6,7 +6,11 @@ import React from "react";
 import {
   HeaderWrapper,
   Logo,
-  Nav
+  Nav,
+  NavItem,
+  NavSearch,
+  Addition,
+  Button
 } from "./style";
 
 class Header extends React.Component {
@@ -15,7 +19,15 @@ class Header extends React.Component {
       <HeaderWrapper>
         <Logo />
         <Nav>
-
+          <NavItem className="left active">首页</NavItem>
+          <NavItem className="left">下载 App</NavItem>
+          <NavItem className="right">登录</NavItem>
+          <NavItem className="right">Aa</NavItem>
+          <NavSearch />
+          <Addition>
+            <Button className="wrtting">写文章</Button>
+            <Button className="reg">注册</Button>
+          </Addition>
         </Nav>
       </HeaderWrapper>
     )
--- src/common/ header/style.js
+++ src/common/ header/style.js
@@ -24,5 +24,67 @@ export const Nav = styled.div`
   width: 960px;
   height: 100%;
   margin: 0 auto;
-  background: red;
+`;
+// padding-right: 70px;
+// box-sizing: border-box;
+
+export const NavItem = styled.div`
+  padding: 0 15px;
+  line-height: 56px;
+  font-size: 17px;
+  color: #333;
+  &.left {
+    float: left;
+  }
+  &.right {
+    float: right;
+    color: #969696;
+  }
+  &.active {
+    color: #ea6f5a;
+  }
+`;
+
+export const NavSearch = styled.input.attrs({
+  placeholder: "搜索"
+})`
+  width: 160px;
+  height: 38px;
+  font-size: 14px;
+  border: 1px solid #eee;
+  border-radius: 19px;
+  background: #eee;
+  outline: none;
+  margin-top: 9px;
+  margin-left: 20px;
+  padding: 0 20px;
+  box-sizing: border-box;
+  &::placeholder {
+    color: #999;
+  }
+`;
+
+export const Addition = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 56px;
+`;
+
+export const Button = styled.div`
+  float: right;
+  line-height: 38px;
+  border-radius: 19px;
+  margin-top: 9px;
+  border: 1px solid rgba(236,97,73,.7);
+  padding: 0px 20px;
+  margin-right: 20px;
+  font-size: 14px;
+  &.reg {
+    color: #ea6f5a;
+  }
+  &.wrtting {
+    color: #fff;
+    background: #ea6f5a;
+  }
 `;
