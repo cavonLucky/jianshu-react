8cb2b69d1c5c647ce1d6e8d6b844861674b0201c
cavonLucky <425247833@qq.com>
2022-04-14 19:32:10 +0800

    7-9使用immutable.js来管理store中的数据

--- src/common/ header/store/redurce.js
+++ src/common/ header/store/redurce.js
@@ -1,19 +1,24 @@
 
 import * as constants from "./constants";
+import { fromJS } from "immutable";
 
-const defaultState = {
+/**
+ * immutable对象不可更改
+ * set(): 会结合之前 immutable 对象的值和设置的值，返回一个全新的对象
+ */
+const defaultState = fromJS({
   focused: false
-};
+});
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = defaultState, action) => {
 
   if (action.type === constants.HEADER_SEARCH_INPUT_FOCUS) {
-    return { focused: true };
+    return state.set("focused", true);
   }
 
   if (action.type === constants.HEADER_SEARCH_INPUT_BLUR) {
-    return { focused: false };
+    return state.set("focused", false);
   }
 
   return state;
